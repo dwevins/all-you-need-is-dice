910e003745bc5ef634f5caf8eb2ae19b35178fb6
Woody Evins <woody.evins@gmail.com>
2018-08-12 21:47:47 -0500

    add results panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import ResultsPanel from './ResultsPanel';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentResult: 0
+    }
+  }
+
   roll20() {
     const roll = Math.ceil((Math.random() * 100) / 5);
-    console.log(roll);
-
+    this.setState({currentResult: roll});
   }
+
   render() {
     return (
       <div className="App">
         <button onClick={ () => this.roll20() }>roll a 20</button>
+        <ResultsPanel result={this.state.currentResult}/>
       </div>
     );
   }
---
+++ src/ResultsPanel.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ResultsPanel(props) {
+    return (
+        <h1>{ props.result }</h1>
+    )
+}
