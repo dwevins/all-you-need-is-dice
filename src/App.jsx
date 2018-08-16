18234395a692651ced3e051e284ea10e19f84eb7
Woody Evins <woody.evins@gmail.com>
2018-08-15 23:24:25 -0500

    DiceSet refactor WIP

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,29 @@ class App extends Component {
     super(props);
 
     this.state = {
-      diceSets: [
-        {key: 1}
-      ]
+      diceSets: []
     }
   }
 
   addSet() {
     const setsCopy = this.state.diceSets.slice();
     const numSets = setsCopy.length;
-    const newKey = setsCopy[numSets - 1].key + 1
-    setsCopy.push({key: newKey})
+    const newKey = numSets > 0 ? this.state.diceSets[numSets - 1].key + 1 : 1;
+    const newSet = {
+      key: newKey,
+      rollData: {
+        2: 0,
+        3: 0,
+        4: 0,
+        6: 0,
+        8: 0,
+        10: 0,
+        12: 0,
+        20: 0,
+      },
+      currentResult: 0
+    }
+    setsCopy.push(newSet)
     this.setState({diceSets: setsCopy});
   }
 
@@ -25,11 +37,67 @@ class App extends Component {
     if (this.state.diceSets.length === 1) return false;
 
     const setsCopy = this.state.diceSets.slice();
-    const targetSet = setsCopy.filter((set) => set.key === key)[0];
+    const targetSet = this.getSet(setsCopy, key);
     if (targetSet) {
       setsCopy.splice(setsCopy.indexOf(targetSet), 1);
       this.setState({diceSets: setsCopy});
     }
+  }
+
+  getSet(sets, key) {
+    const targetSet = sets.filter((set) => set.key === key)
+    return targetSet.length ? targetSet[0] : null;
+  }
+
+  roll(key) {
+    const setsCopy = this.state.diceSets.slice();
+    const targetSet = this.getSet(setsCopy, key);
+    const newSet = {};
+    if (targetSet) {
+      let total = 0;
+      const keys = Object.keys(targetSet.rollData);
+
+      for (let i = 0; i < keys.length; i++) {
+        const curDie = keys[i];
+        const numRolls = targetSet.rollData[curDie];
+        const mod = Math.floor(100 / curDie);
+
+        for (let j = 1; j <= numRolls; j++) {
+          let roll = Math.ceil((Math.random() * 100) / mod);
+          total += roll;
+        }
+      }
+
+      newSet.key = targetSet.key;
+      newSet.rollData = targetSet.rollData;
+      newSet.total = total;
+    }
+
+    setsCopy.splice(setsCopy.indexOf(targetSet), 0, newSet);
+    this.setState({diceSets: setsCopy});
+  }
+
+  addDie(key, die) {
+    const setsCopy = this.state.diceSets.slice();
+    const targetSet = this.getSet(key);
+    if (targetSet) {
+      setsCopy[setsCopy.indexOf(targetSet)].rollData[die] += 1;
+      this.setState({diceSets: setsCopy});
+    }
+  }
+
+  dropDie(key, die) {
+      const setsCopy = this.state.diceSets.slice();
+      const targetSet = this.getSet(key);
+      if (targetSet) {
+        const rollData = setsCopy[setsCopy.indexOf(targetSet)].rollData;
+        setsCopy[setsCopy.indexOf(targetSet)].rollData[die] = rollData[die] > 0 ? rollData[die] - 1 : 0;
+        this.setState({diceSets: setsCopy});
+      }
+  }
+
+  componentDidMount() {
+    this.addSet();
   }
 
   render() {
@@ -42,7 +110,14 @@ class App extends Component {
         </header>
         <div className="sets-container content-container">
           { this.state.diceSets.map((set) =>
-            <DiceSet key={ set.key } drop={() => this.dropSet(set.key)}/>
+            <DiceSet
+              key={ set.key }
+              rollData = { set.rollData }
+              drop={ () => this.dropSet(set.key) }
+              roll={ () => this.roll(set.key) }
+              addDie={ (die) => this.addDie(set.key, die) }
+              dropDie={ (die) => this.dropDie(set.key, die) }
+            />
           )}
         </div>
       </div>
--- src/DiceSet.jsx
+++ src/DiceSet.jsx
@@ -1,76 +1,25 @@
 import React from 'react';
 
-export default class DicePanel extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rollData: {
-                2: 0,
-                3: 0,
-                4: 0,
-                6: 0,
-                8: 0,
-                10: 0,
-                12: 0,
-                20: 0,
-            },
-            currentResult: 0
-        }
-    }
-
-    addDie(die) {
-        const rollDataCopy = this.state.rollData;
-        rollDataCopy[die] += 1;
-        this.setState({rollData: rollDataCopy});
-    }
-
-    dropDie(die) {
-        const rollDataCopy = this.state.rollData;
-        rollDataCopy[die] = rollDataCopy[die] > 0 ? rollDataCopy[die] - 1 : 0;
-        this.setState({rollData: rollDataCopy});
-    }
-
-    renderDie(die, key) {
-        return (
-            <div className="die-container" key={key}>
-                <button onClick={ () => this.addDie(die) }>Add</button>
-                <button onClick={ () => this.dropDie(die) }>Drop</button>
-                <p className="count">d{ die } x {this.state.rollData[die]}</p>
-            </div>
-        )
-    }
-
-    roll(rollData) {
-        const keys = Object.keys(rollData);
-        let total = 0;
-
-        for (let i = 0; i < keys.length; i++) {
-          const curDie = keys[i];
-          const numRolls = rollData[curDie];
-          const mod = Math.floor(100 / curDie);
-
-          for (let j = 1; j <= numRolls; j++) {
-            let roll = Math.ceil((Math.random() * 100) / mod);
-            total += roll;
-          }
-        }
-        this.setState({currentResult: total});
-      }
-
-    render() {
-        return (
-            <div className="dice-set">
-                <div className="controls">
-                    <div className="dice-menu">
-                        {Object.keys(this.state.rollData).map((die, key) => this.renderDie(die, key))}
-                    </div>
-                    <button onClick={ () => this.roll(this.state.rollData) }>Roll</button>
-                    <button onClick={ () => this.props.drop() }>Drop Set</button>
+export default function DicePanel(props) {
+    console.log(props);
+    return (
+        <div className="dice-set">
+            <div className="controls">
+                <div className="dice-menu">
+                    {Object.keys(props.rollData).map((die, key) =>
+                        <div className="die-container" key={key}>
+                            <button onClick={ () => props.addDie(die) }>Add</button>
+                            <button onClick={ () => props.dropDie(die) }>Drop</button>
+                            <p className="count">d{ die } x {props.rollData[die]}</p>
+                        </div>
+                    )}
                 </div>
-                <section className="results">
-                    <h3 className="result">{ this.state.currentResult }</h3>
-                </section>
+                <button onClick={ () => props.roll() }>Roll</button>
+                <button onClick={ () => props.drop() }>Drop Set</button>
             </div>
-        )
-    }
+            <section className="results">
+                <h3 className="result">{ props.result }</h3>
+            </section>
+        </div>
+    )
 }
