904d2d79c91d70299b792cd8fa4807f8ea2c7200
Woody Evins <woody.evins@gmail.com>
2018-09-16 20:40:50 -0500

    Refactor die controls layout

--- src/DiceSet.jsx
+++ src/DiceSet.jsx
@@ -3,21 +3,19 @@ import React from 'react';
 export default function DicePanel(props) {
     return (
         <div className="dice-set">
-            <div className="controls">
-                <div className="dice-menu">
-                    {Object.keys(props.rollData).map((die, key) =>
-                        <div className="die-container" key={key}>
-                            <p className="count">d{ die } x {props.rollData[die]}</p>
-                            <button onClick={ () => props.addDie(die) }>Add</button>
-                            <button onClick={ () => props.dropDie(die) }>Drop</button>
-                        </div>
-                    )}
-                </div>
-                <button onClick={ () => props.roll() }>Roll</button>
+            <div className="dice-menu">
                 <button className="drop-btn" onClick={ () => props.drop() }>X</button>
+                {Object.keys(props.rollData).map((die, key) =>
+                    <div className="die-container" key={key}>
+                        <p className="count">d{ die } x {props.rollData[die]}</p>
+                        <button className="die-control" onClick={ () => props.addDie(die) }>+</button>
+                        <button className="die-control" onClick={ () => props.dropDie(die) }>-</button>
+                    </div>
+                )}
             </div>
             <section className="results">
-                <h3 className="result">{ props.result }</h3>
+                <h2 className="result">{ props.result }</h2>
+                <button onClick={ () => props.roll() }>Roll</button>
             </section>
         </div>
     )
