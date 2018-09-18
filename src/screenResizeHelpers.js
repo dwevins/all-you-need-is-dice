0828afdb26e8580ce145ccbbc5a7049d394b9e8c
Woody Evins <woody.evins@gmail.com>
2018-09-17 22:07:44 -0500

    Add screen resize helpers

---
+++ src/screenResizeHelpers.js
@@ -0,0 +1,30 @@
+let screenModeFlagEl = null;
+let screenModeFlag = null;
+let uiMode = null;
+const callbacks = [];
+
+function init() {
+    screenModeFlagEl = document.getElementById('ui-mode');
+    screenModeFlag = window.getComputedStyle(screenModeFlagEl, ':after');
+    uiMode = screenModeFlag.content;
+
+    window.addEventListener('resize', () => {
+        // quote removal solution from https://stackoverflow.com/questions/19156148/i-want-to-remove-double-quotes-from-a-string
+        const flag = screenModeFlag.content.replace(/"/g,"");
+        if (uiMode !== flag) {
+            runCallbacks(callbacks.filter(cb => cb.mode === `${flag}`));
+            uiMode = flag;
+        }
+    })
+}
+
+function runCallbacks (filteredCallbacks) {
+    for (let i = 0; i < filteredCallbacks.length; i++) {
+        filteredCallbacks[i].callback();
+    }
+}
+
+export function registerCallback(mode = 'mobile', callback = () => {}) {
+    if (!callbacks.length) init();
+    callbacks.push({ mode, callback });
+}
