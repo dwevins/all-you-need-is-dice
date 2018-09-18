8c00ff76b3e8351b2b806f3aa4f39a7ac82eaeb2
Woody Evins <woody.evins@gmail.com>
2018-09-17 22:09:22 -0500

    Add groundwork for menu system switching

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { registerCallback } from './screenResizeHelpers';
 
 class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            navMode: 'desktop'
+        }
+    }
+
+    componentDidMount() {
+        registerCallback('mobile', () => this.setState({navMode: 'mobile'}))
+        registerCallback('desktop', () => this.setState({navMode: 'desktop'}))
+    }
+
+
     render() {
         return (
             <div className="set-controls">
-                <button onClick={ () => this.props.addEmptySet() }><span>Add a Set</span></button>
-                <button onClick={ () => this.props.rollAll() }><span>Roll All</span></button>
+            {this.state.navMode === 'desktop' && (
+                <Fragment>
+                    <button onClick={ () => this.props.addEmptySet() }><span>Add a Set</span></button>
+                    <button onClick={ () => this.props.rollAll() }><span>Roll All</span></button>
+                </Fragment>
+
+            )}
+            {this.state.navMode === 'mobile' &&
+                <Fragment>
+                    <button onClick={ () => this.props.addEmptySet() }><span>MOBILE Add a Set</span></button>
+                    <button onClick={ () => this.props.rollAll() }><span>MOBILE Roll All</span></button>
+                </Fragment>
+            }
             </div>
         )
     }
